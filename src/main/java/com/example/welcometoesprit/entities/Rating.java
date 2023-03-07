cade98c89d4aa96186aa9f7fdcf75ca2aa4849de
Ghassèn Bahry <92761242+GastonAZ5@users.noreply.github.com>
2023-03-07 14:25:59 +0100

    badword detecting in comments publications rating chat configuration

---
+++ src/main/java/com/example/welcometoesprit/entities/Rating.java
@@ -0,0 +1,26 @@
+package com.example.welcometoesprit.entities;
+
+import lombok.*;
+
+import jakarta.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "Rating")
+public class Rating implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idRating;
+
+    private Integer score;
+
+    @ManyToOne
+    private Publication publication;
+
+    @ManyToOne
+    private User user;
+}
